717a5ddc3ad171bcdedfbda6c3ca15984fec2049
HardMCore <hardmcore@hotmail.com>
2019-08-04 18:05:10 +0200

    NoItemDamage 2.0

--- src/me/hardmcore/ListenerItemDamage.java
+++ src/me/hardmcore/ListenerItemDamage.java
@@ -1,12 +1,23 @@
 package me.hardmcore;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 
 public class ListenerItemDamage implements Listener {
+
 	@EventHandler
 	public void onPlayerItemDamageEvent(PlayerItemDamageEvent event) {
-		event.setCancelled(true);
+		Player player = event.getPlayer();
+		if (MainClass.noitemdmg == true) {
+			if(player.hasPermission("noitemdmg")) {
+				event.setCancelled(true);
+				return;
+			}
+			if(player.isOp()) {
+				event.setCancelled(true);
+			}
+		}
 	}
 }
--- src/me/hardmcore/MainClass.java
+++ src/me/hardmcore/MainClass.java
@@ -1,17 +1,38 @@
 package me.hardmcore;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MainClass extends JavaPlugin {
+public class MainClass extends JavaPlugin implements CommandExecutor {
+	static boolean noitemdmg = true;
 	public void onEnable() {
 		PluginManager manager = this.getServer().getPluginManager();
-		
 		manager.registerEvents(new ListenerItemDamage(), this);
-		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "NoItemDamage turned on");
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Plugin NoItemDamage turned on");
 	}
 	public void onDisable() {
-		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Wylaczono NoItemDamage turned off");
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Plugin NoItemDamage turned off");
 	}
+    public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args){
+        if(cmd.getName().equalsIgnoreCase("noitemdmg")){
+            if(args.length == 0){
+                sender.sendMessage(ChatColor.GRAY + "/noitemdmg on - Prevents items from taking damage");
+                sender.sendMessage(ChatColor.GRAY + "/noitemdmg off - Items take damage");
+                return true;
+            }
+            if(args[0].equalsIgnoreCase("on")) {
+            	noitemdmg = true;
+            	sender.sendMessage(ChatColor.GRAY + "Items don't take damage anymore");
+            }
+            if(args[0].equalsIgnoreCase("off")) {
+            	noitemdmg = false;
+            	sender.sendMessage(ChatColor.GRAY + "Items now take damage again");
+            }
+        }
+        return false;
+    }
 }
